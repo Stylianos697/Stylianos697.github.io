2140fd825fe233073b025e22b52efc537800654b
Stylianos Gavriel <stelgabriel.com>
2024-09-29 20:05:04 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("showPhone").addEventListener("click", function() {
+    alert("Phone number: +375 96526532");
+});
+
+var div = document.getElementById("header");
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColor(){
+  div.style.backgroundColor= getRandomColor();
+}
+
+setInterval(changeColor,1000);
